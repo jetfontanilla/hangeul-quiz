1697f383e975d829d6c004526093c6d34f7ab69c
Jet Fontanilla <jetlogs@gmail.com>
2024-07-30 14:00:24 +0900

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.vue',
-  ],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
+
